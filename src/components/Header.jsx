ab8724c6e3e32a0bfde27d2cc487b121fb124776
Moy Haobei <haobei.moy@vitrox.com>
2024-08-26 15:49:32 +0800

    added Modal and cart

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,18 @@
 import { useContext } from "react";
 import logoImg from "../assets/logo.jpg"
 import Button from "./UI/Button";
+import UserProgressContext from "../store/UserProgressContext";
 import CartContext from "../store/CartContext";
 
 export default function Header() {
   const cartCtx = useContext(CartContext);
+  const userProgressCtx = useContext(UserProgressContext);
 
   const totalCartItems = cartCtx.items.reduce((total, item) => total + item.quantity, 0);
+
+  function handleShowCart() {
+    userProgressCtx.showCart();
+  }
 
   return (
     <header id="main-header">
@@ -15,7 +21,7 @@ export default function Header() {
         <h1>React Food App</h1>
       </div>
       <nav>
-        <Button textOnly>Cart ({totalCartItems})</Button>
+        <Button textOnly onClick={handleShowCart}>Cart ({totalCartItems})</Button>
       </nav>
     </header>
   );
