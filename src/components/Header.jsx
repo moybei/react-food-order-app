12956527e2cd579c42c97f46448dc0b8aa78e68c
Moy Haobei <haobei.moy@vitrox.com>
2024-08-26 14:53:12 +0800

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import { useContext } from "react";
+import logoImg from "../assets/logo.jpg"
+import Button from "./UI/Button";
+import CartContext from "../store/CartContext";
+
+export default function Header() {
+  const cartCtx = useContext(CartContext);
+
+  const totalCartItems = cartCtx.items.reduce((total, item) => total + item.quantity, 0);
+
+  return (
+    <header id="main-header">
+      <div id="title">
+        <img src={logoImg} alt="Food Logo Image" />
+        <h1>React Food App</h1>
+      </div>
+      <nav>
+        <Button textOnly>Cart ({totalCartItems})</Button>
+      </nav>
+    </header>
+  );
+}
